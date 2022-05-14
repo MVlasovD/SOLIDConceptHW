ed5216a5bd5f77e045d01cb5b9702cceacd762c7
Mikhail Vlasov <m.vlasov@dunice.net>
2022-05-14 18:30:55 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+=======
+
+
+
+>>>>>>> coding
+        System.out.println("Hello world!");
+    }
+}
