33abca81ed07f43a2a6ce50bb5a54b8f8b5c2b46
Mikhail Vlasov <m.vlasov@dunice.net>
2022-05-14 18:33:12 +0300

    coding

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> coding
         System.out.println("Hello world!");
     }
 }
